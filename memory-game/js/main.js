1878d5d1448f417d67c51631d40a2ba78cc0bc12
Elisa <alisa260785@gmail.com>
2020-02-21 16:46:34 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,9 @@
-console.log ("Up and running <3!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log ("User flipped " + cardOne)
-console.log ("User flipped " + cardThree)
+const cards = ["queen", "king", "queen", "king"];
+const cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped queen");
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("user flipped king");
+if (cardsInPlay.length === 2) {if (cardOne === cardTwo){console.log("match")} else (console.log("looser, pick another card"));} else (console.log("pick another card"));
