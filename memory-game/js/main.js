5098c3ccb0077656670bfa461e9a4199fb616f41
Elisa <alisa260785@gmail.com>
2020-02-28 16:16:08 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,20 @@
-const cards = ["queen", "king", "queen", "king"];
-const cardsInPlay = [];
-const cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped queen");
-const cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped king");
-if (cardsInPlay.length === 2) {if (cardOne === cardTwo){console.log("match")} else (console.log("looser, pick another card"));} else (console.log("pick another card"));
+// const cards = ["queen", "queen", "king", "king"];
+// const cardsInPlay = [];
+
+// const checkForMatch = () => {
+//   if (cardsInPlay.length === 2) {
+//     if (cardsInPlay[0] === cardsInPlay[1]) {
+//       console.log("You found a match!");
+//     } else {
+//       console.log("Sorry, try again.");
+//     }
+//   } else console.log("Pick another card");
+// };
+// function flipCard(cardId) {
+//   console.log("User flipped " + cards[cardId]);
+//   cardsInPlay.push(cards[cardId]);
+//   checkForMatch();
+// }
+
+// flipCard(0);
+// flipCard(2);
