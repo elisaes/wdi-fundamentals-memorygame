b58f84a3b2ca2c9629a342e8885a2e251c6a3839
Elisa <alisa260785@gmail.com>
2020-03-10 21:19:20 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,43 +1,66 @@
 const cards = [
   {
-    rank:"queen",
-    suit:"hearts",
-    cardImage: "images/queen-of-hearts.png",
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
   },
   {
     rank: "queen",
     suit: "diamonts",
-    cardImage: "images/queen-of-diamonds.png",
+    cardImage: "images/queen-of-diamonds.png"
   },
   {
     rank: "king",
     suit: "hearts",
-    cardImage: "images/king-of-hearts.png",
+    cardImage: "images/king-of-hearts.png"
   },
   {
     rank: "king",
     suit: "diamonts",
-    cardImage:"images/king-of-diamonds.png",
+    cardImage: "images/king-of-diamonds.png"
   }
 ];
-const cardsInPlay = [];
+let cardsInPlay = [];
 
 const checkForMatch = () => {
+  console.log(cardsInPlay);
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert("You found a match!");
     } else {
       alert("Sorry, try again.");
     }
-  } else console.log("Pick another card");
+  } else alert("Pick another card");
 };
-function flipCard(cardId) {
+function flipCard() {
+  const cardId = this.getAttribute("data-id");
+  this.setAttribute("src", cards[cardId].cardImage);
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
+  console.log(cards[cardId].suit)
   checkForMatch();
 }
+function createBoard() {
+  for (let i = 0; i < cards.length; i++) {
+    const cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+}
+function resetCards() {
+  for (let j = 0; j < cards.length; j ++) {
+  const cardBack = document.getElementsByTagName("img")[j];
+  cardBack.setAttribute("src","images/back.png");
+  cardsInPlay = [];
+      }
+  console.log(cards);
+  const resetSort = document.getElementById("reset");
+  resetSort.addEventListener("click", resetCards);
 
-flipCard(0);
-flipCard(2);
+}
+
+createBoard();
+resetCards();
--- memory-game/js/test.js
+++ memory-game/js/test.js
@@ -22,7 +22,7 @@
 /////////////   TEST     ///////
 
 const school = {
-  teachers: { Sue: "1A", Joe: ["2B", "3C"] },
+  teachers: { Sue: "1A", Joe: ["2B", "3C", "4D"] },
   classes: {
     "1A": {
       students: [
@@ -41,11 +41,63 @@ const school = {
         { name: "Zoey", gender: "female" },
         { name: "Yelle", gender: "male" }
       ]
+    },
+    "4D": {
+      students: [
+        { name: "Eee", gender: "female" },
+        { name: "Fff", gender: "male" }
+      ]
     }
   }
 };
 
 // Q1. Console log all students in Sue's class
-
-
+for (let i = 0; i < school.classes["1A"].students.length; i++) {
+  const sueClasses = school.classes["1A"];
+  sueStudentsName = sueClasses.students[i].name;
+  sueStudentsGender = sueClasses.students[i].gender;
+  console.log(
+    school.teachers.Sue + " " + sueStudentsName + " " + sueStudentsGender
+  );
+}
 // Q2. Console log all students in Joe's classes
+// const joeStudentsTwoB = school.classes["2B"].students;
+// const joeStudentsThreeC = school.classes["3C"].students;
+// console.log(joeStudentsTwoB);
+// console.log(joeStudentsThreeC);
+// for (let i = 0; i < school.teachers.Joe.length; i++) {
+//   console.log("here", school.teachers.Joe.length,i)
+//   for (let j = 0; j < joeStudentsTwoB.length; j++) {
+//     console.log(
+//       school.teachers.Joe[i] +
+//         " " +
+//         joeStudentsTwoB[j].name +
+//         " " +
+//         joeStudentsTwoB[j].gender
+//     );
+//   }
+//   if ((j = joeStudentsTwoB.length - 1)) {
+//     i = i + 1;
+//   }
+//   for (let k = 0; k < joeStudentsThreeC.length; k++) {
+//     console.log(
+//       school.teachers.Joe[i] +
+//         " " +
+//         joeStudentsThreeC[k].name +
+//         " " +
+//         joeStudentsThreeC[k].gender
+//     );
+//   }
+// }
+console.log("test 2222222222222")
+for (let i = 0; i < school.teachers.Joe.length; i++) {
+
+  const cla = school.teachers.Joe[i]; // this get each class of Joe
+  console.log(cla)
+  const studentsInClass = school.classes[cla].students;
+
+  for (let j = 0; j < studentsInClass.length; j++) {
+    const student = studentsInClass[j];
+   // console.log(cla + " " + student.name + " " + student.gender);
+  }
+}
